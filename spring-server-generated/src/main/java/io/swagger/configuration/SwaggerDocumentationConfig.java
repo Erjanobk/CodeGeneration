d625f93dfa21d19773b6a82f1cbb764904d25ed2
Erjanobk <54842730+Erjanobk@users.noreply.github.com>
2021-05-27 15:26:57 +0200

    Erjano

--- spring-server-generated/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ spring-server-generated/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -54,5 +54,5 @@ public class SwaggerDocumentationConfig {
                 .contact(new io.swagger.v3.oas.models.info.Contact()
                     .email("632901@student.inholland.nl")));
     }
-
+///
 }
