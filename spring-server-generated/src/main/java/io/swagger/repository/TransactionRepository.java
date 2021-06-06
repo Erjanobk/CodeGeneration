fae3cd292187f1532886f575cf3c9e71c4284461
Erjanobk <54842730+Erjanobk@users.noreply.github.com>
2021-06-06 22:09:20 +0200

    qrf

---
+++ spring-server-generated/src/main/java/io/swagger/repository/TransactionRepository.java
@@ -0,0 +1,20 @@
+package io.swagger.repository;
+
+import io.swagger.model.Transactions;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import org.threeten.bp.OffsetDateTime;
+
+import io.swagger.model.Transactions;
+import javax.validation.constraints.DecimalMin;
+import java.math.BigDecimal;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transactions, Integer> {
+
+    Transactions getAllTransactions(Integer id, String from, String to, double amount, String message, String userPerforming, OffsetDateTime transactionDate);
+    Transactions getTransactionByID(Integer transactionID);
+    Transactions getTransactionByParam(String userID, OffsetDateTime startDate, OffsetDateTime endDate);
+    Transactions createTransaction(Transactions t);
+}
